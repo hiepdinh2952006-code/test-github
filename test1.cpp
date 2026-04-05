582f4e2e1b79ceb800c883ae908ba9879f8d83f3
quyetvongtb2006 <logiaquyetvt@gmail.com>
2026-04-05 12:09:53 +0700

    Add initial implementation of test1 program

---
+++ test1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+        cout << i << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
